9cdc0ddb797f62471af5892da174414cf508fda1
unknown <ryanjsiemens@gmail.com>
2015-05-29 15:06:10 -0700

    Set up dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#Install and enable mod_wsgi
+apt-get install libapache2-mod-wsgi python-dev 2>> setup-error.log
+a2enmod wsgi 2>> setup-error.log
 
+#Setup virtualenv and install all dependencies
+apt-get install python-pip 2>> setup-error.log
+pip install virtualenv 2>> setup-error.log
+virtualenv venv 2>> setup-error.log
+source venv/bin/active 2>> setup-error.log
+pip install -r requirments.txt 2>> setup-error.log
+
+#Check for db files and backup directory
 if [ ! -f app.db ];
 then
   touch app.db
@@ -18,8 +29,9 @@ then
   echo "Made backup directory $(readlink -f ../app-backups)"
 fi
 
+#Set cronjob to backup db daily at midnight
 crontab -l > mycron
 echo "0 0 * * * backup.sh" >> mycron
-crontab mycron
+crontab mycron 2>> setup-error.log
 rm mycron
 echo "Scheduled backups for once a day at midnight in $(readlink -f ../app-backups)"
