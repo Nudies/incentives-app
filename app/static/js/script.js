ffd32fec96314f1321955b4eb94b73abbc1a0782
Ryan Siemens <ryanjsiemens@gmail.com>
2015-07-03 12:32:30 -0700

    Removed custom nav, added bootstrap nav

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,56 +1,10 @@
 $(document).ready(function(){
 
-  //auto adjust #content left margin depending on nav width.
-  var $nav = $('#nav').outerWidth();
-  var $content = $('#content');
-
-
-  if($(window).width() > 768){ //css breakpoint
-    $content.css({'margin-left': $nav});
-  }
-  else{
-    $content.css({'margin-left': 0});
-  }
-
-
-  //listen for window resize and adjust again.
-  $(window).resize(function(e){
-    $nav = $('#nav').outerWidth();
-    $content = $('#content');
-    if($(window).width() > 768){
-      $content.css({'margin-left': $nav});
-      $('#nav').css({'display': 'block'});
-    }
-    else{
-      $content.css({'margin-left': 0});
-      $('#nav').css({'display': 'none'});
-
-      $('#mobile-nav').on('click', function(e){
-        var $dispType = $('#nav').css('display');
-        if($dispType === 'none'){
-          $('#nav').css({'display': 'block'});
-        }
-        else if($dispType === 'block'){
-          $('#nav').css({'display': 'none'});
-        }
-      });
-    }
+  $(".nav li").on("click", function() {
+    $(".nav li").removeClass("active");
+    $(this).addClass("active");
   });
 
-
-  //Mobile nav toggle
-  if($(window).width() < 769){
-    $('#mobile-nav').on('click', function(e){
-      var $dispType = $('#nav').css('display');
-      if($dispType === 'none'){
-        $('#nav').css({'display': 'block'});
-      }
-      else if($dispType === 'block'){
-        $('#nav').css({'display': 'none'});
-      }
-    });
-  }
-
-
   $('#date').datepicker();
+
 });
